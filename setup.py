9798abe89adc0ffc447b4ebc4649f7e0aaf61c19
Michael Orr <michael@orr.co>
2014-09-15 15:53:37 -0400

    tweaks for distribution

--- setup.py
+++ setup.py
@@ -1,30 +1,28 @@
 from setuptools import setup, find_packages
 
-version = '1.0.0'
+version = '1.1.0'
 
-setup(name="helga-spook",
+setup(name='helga-spook',
       version=version,
       description=('prints nsa buzzwords'),
-      classifiers=['Development Status :: 1 - Beta',
-                   'Environment :: IRC',
-                   'Intended Audience :: Twisted Developers, IRC Bot Developers',
+      classifiers=['Development Status :: 4 - Beta',
                    'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
                    'Operating System :: OS Independent',
                    'Programming Language :: Python',
                    'Topic :: Software Development :: Libraries :: Python Modules',
-                   'Topic :: IRC Bots'],
+                   'Topic :: Communications :: Chat :: Internet Relay Chat'],
       keywords='irc bot nsa spook emacs',
-      author='michael orr',
+      author='Michael Orr',
       author_email='michael@orr.co',
       url='https://github.com/michaelorr/helga-spook',
       license='LICENSE',
       packages=find_packages(),
       include_package_data=True,
-      py_modules=['helga-spook'],
+      py_modules=['helga_spook'],
       zip_safe=True,
       entry_points = dict(
-          helga_plugins = [
-              'spook= helga_spook:spook',
+          helga_plugins=[
+              'spook = helga_spook:spook',
           ],
       ),
 )
